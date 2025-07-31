f1679d09100be685c27645e3df889a1251770bd8
Ario Waskita <web.designer@loanmarket.co.id>
2025-07-31 16:06:14 +0700

    feat(components): sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
-import { Menu } from "antd";
+import { Button, Dropdown, Menu } from "antd";
 import {
   UserOutlined,
   AppstoreOutlined,
   SettingOutlined,
   FileTextOutlined,
   MenuUnfoldOutlined,
-  MenuOutlined
+  MenuOutlined,
+  LogoutOutlined
 } from "@ant-design/icons";
 import './sidebar-custom.css';
+import { useNavigate } from "react-router-dom";
 
 const items = [
   {
@@ -38,11 +40,28 @@ const items = [
 ];
 
 export default function Sidebar({ selectedKey = "1", onSelect }) {
+  const navigate = useNavigate();
+  
   return (
     <div className="h-full flex flex-col bg-white shadow-sm">
       <div className="px-6 py-4 mb-4 flex items-center justify-between gap-2">
         <img src="/images/logo.png" alt="logo" className="w-24" />
-        <MenuOutlined />
+
+        <Dropdown menu={{ 
+          items: [
+            {
+              key: "logout",
+              label: <Button icon={<LogoutOutlined />} type="link" className="!text-red-500" onClick={() => {
+                localStorage.clear()
+                setTimeout(() => {
+                  navigate('/login')
+                }, 1000);
+              }}>Logout</Button>
+            }
+          ]
+         }} placement="bottomLeft" arrow>
+          <MenuOutlined />
+        </Dropdown>
       </div>
 
       <Menu
