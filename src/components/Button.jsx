91ee42e8a2a2857b7f4d067efdfac35fa118b6e7
Ario Waskita <web.designer@loanmarket.co.id>
2025-07-31 16:05:57 +0700

    feat(compoennts): button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button (props) {
     return (
-        <button className={`${props.size} px-8 py-2 ${props.variant} rounded-lg`} type={props.type} onClick={props.onClick}>{props.text}</button>
+        <button disabled={props.disabled} className={`${props.size} px-8 py-2 ${props.variant} rounded-lg`} type={props.type} onClick={props.onClick}>{props.text}</button>
     )
 }
