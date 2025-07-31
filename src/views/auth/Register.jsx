58411f2878a9c2136b410817a004cbc146c8946b
daffa tabiano <daffatabianoo@gmail.com>
2025-07-31 07:12:42 +0700

    feat(auth): register

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,9 +1,16 @@
-export default function Register () {
-    return (
-        <main>
-            <section>
-                
-            </section>
-        </main>
-    )
+import AuthLayout from "../../layouts/AuthLayout";
+import UserForm from "../../components/forms/UserForm";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const navigate = useNavigate();
+  return (
+    <AuthLayout variant="register">
+      <div className="w-full max-w-2xl">
+        <h1 className="mb-8 text-3xl font-bold">Daftar</h1>
+
+        <UserForm onSuccess={() => navigate('/')}/>
+      </div>
+    </AuthLayout>
+  );
 }
