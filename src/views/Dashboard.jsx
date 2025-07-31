ed41a68508be1feb3def9efa91dbbe419d98c27a
daffa tabiano <daffatabianoo@gmail.com>
2025-07-31 07:11:29 +0700

    feat(views): dashboard user lists fetch

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,13 +1,10 @@
+import UserAktif from "../components/tables/UserAktif";
+import DashboardLayout from "../layouts/DashboardLayout";
+
 export default function Dashboard () {
     return (
-        <main className="w-full h-full flex flex-col sm:flex-row ">
-            <section className="w-full relative">
-                running tailwind css
-            </section>
-
-            <section className="w-full">
-                running tailwind css
-            </section>
-        </main>
+        <DashboardLayout>
+            <UserAktif />
+        </DashboardLayout>
     )
 }
