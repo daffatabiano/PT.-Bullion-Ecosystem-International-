1d09d60e34d09fce7134fd7113b8829a5992d4ab
Ario Waskita <web.designer@loanmarket.co.id>
2025-07-31 16:08:44 +0700

    feat(auth):register

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
       <div className="w-full max-w-2xl">
         <h1 className="mb-8 text-3xl font-bold">Daftar</h1>
 
-        <UserForm onSuccess={() => navigate('/')}/>
+        <UserForm onSuccess={() => navigate('/')} />
       </div>
     </AuthLayout>
   );
